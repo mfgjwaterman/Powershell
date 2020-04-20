8570acec7b77c93e8bcd24830e334a56ff95afe5
mfgjwaterman <michael.waterman@outlook.com>
2020-04-20 18:01:26 +0200

    Added function example

---
+++ Examples/Functions.ps1
@@ -0,0 +1,16 @@
+﻿function MyFunction (
+    
+[parameter(Mandatory = $true)]
+[int]$param1)
+
+{
+    $Param2 = $param1 + 1
+    $Result = $true
+    return $Result, $Param2
+}
+
+$Result = MyFunction(100)
+Write-host "Array members: " $Result
+Write-host "Array member 1: " $Result[0]
+Write-host "Array member 2: " $Result[1]
+$Result.GetType()
