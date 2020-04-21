24bd55019327c0963c00107f7c4cde633844afb7
mfgjwaterman <michael.waterman@outlook.com>
2020-04-21 18:04:15 +0200

    Changed Test-InternetConnection

--- Functions/Test-InternetConnection.ps1
+++ Functions/Test-InternetConnection.ps1
@@ -4,7 +4,6 @@ Function Test-InternetConnection {
 
     Switch ( Test-NetConnection -CommonTCPPort HTTP -ComputerName $Beacon ){
         { ( $_.TcpTestSucceeded -ne $true ) } { 
-            $ErrorActionPreference = 'Stop'
             Write-Error -Message 'Cannot connect to the internet, please check your connection and try again.'
         }
         { ( $_.TcpTestSucceeded -eq $true ) } { 
