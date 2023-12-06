edf4e8a68bed7a75423f804f575ff35bc4b52ffa
Michael Waterman <47662599+mfgjwaterman@users.noreply.github.com>
2023-12-06 13:32:06 +0100

    Add files via upload

---
+++ Scripts/Convert-PingCastleToCSV.ps1
@@ -0,0 +1,45 @@
+﻿# Parameter input
+##############################################################################################
+[CmdletBinding(DefaultParameterSetName="Default")]
+param(
+[Parameter(
+    Mandatory=$true
+    )]
+[string]$XMLFile,
+[Parameter(
+    Mandatory=$true
+    )]
+[string]$Path
+)
+##############################################################################################
+
+
+# Check input file
+##############################################################################################
+Test-Path $XMLFile -ErrorAction Stop | Out-Null
+##############################################################################################
+
+
+# Check Output diectory
+##############################################################################################
+If(-not (Test-Path $Path) ){
+    New-Item -Path $XMLFile -ItemType Directory -ErrorAction Stop | Out-Null
+}
+##############################################################################################
+
+
+# Process the XML file
+##############################################################################################
+$HealthcheckRiskRules = (Select-Xml -Path $XMLFile -XPath "/HealthcheckData/RiskRules/HealthcheckRiskRule").node
+$CsvObj = $HealthcheckRiskRules | Select-Object -Property "Rationale", "Category", "Points", "Health Impact", "Workload", "Priority", "Actions", "Hours", "Owner", "Status", "Remarks"
+##############################################################################################
+
+
+# Export the file
+##############################################################################################
+$DateTime = (([datetime](Select-Xml -Path $XMLFile -XPath *).node.GenerationDate).DateTime).ToString().replace(':',' ')
+$NetBiosName = ((Select-Xml -Path $XMLFile -XPath *).node.NetBiosName).ToLower()
+$WriteFilePath = (Join-Path -Path $Path -ChildPath $($NetBiosName + " - " + $DateTime + ".csv") )
+
+$CsvObj | Export-Csv -Path $WriteFilePath -NoTypeInformation -Delimiter ';'
+##############################################################################################
