14530f5b2f04f93b426e809ecca9203d21349a4c
Michael Waterman <47662599+mfgjwaterman@users.noreply.github.com>
2024-04-09 15:41:25 +0200

    Add files via upload

--- Scripts/AutomatedLab/Install-SingleEndPoint.ps1
+++ Scripts/AutomatedLab/Install-SingleEndPoint.ps1
@@ -16,13 +16,6 @@ Add-LabMachineDefinition -Name $Name -OperatingSystem 'Windows 10 Enterprise Eva
                          -EnableWindowsFirewall
 Install-Lab
 
-# Reboot
-$LabClients = Get-LabVM | where {( $_.OperatingSystem -NotLike "*Windows Server*" -and $_.OperatingSystemType -eq "Windows" )}
-foreach($LabClient in $LabClients)
-{
-    Restart-LabVM -ComputerName $LabClient.Name -NoNewLine -Wait
-}
-
 # Remove Edge
 Invoke-LabCommand -ComputerName (Get-LabVM) -ActivityName "EdgeRemove" -ScriptBlock {
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ChrisTitusTech/winutil/main/edgeremoval.ps1" -OutFile "edgeremoval.ps1"
@@ -44,13 +37,6 @@ Invoke-LabCommand -ComputerName (Get-LabVM) -ActivityName "Winget" -ScriptBlock 
     Remove-Item -Path "Microsoft.VCLibs.x64.14.00.Desktop.appx"
     Remove-Item -Path "Microsoft.UI.Xaml.2.8.x64.appx"
     Remove-Item -Path "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-}
-
-# Reboot
-$LabClients = Get-LabVM | where {( $_.OperatingSystem -NotLike "*Windows Server*" -and $_.OperatingSystemType -eq "Windows" )}
-foreach($LabClient in $LabClients)
-{
-    Restart-LabVM -ComputerName $LabClient.Name -NoNewLine -Wait
 }
 
 # Download VPN
@@ -103,10 +89,15 @@ Invoke-LabCommand -ComputerName (Get-LabVM) -ActivityName "Cleanup" -ScriptBlock
         if (Test-Path -Path 'C:\WinRmCustomization.ps1'){
         Remove-Item 'C:\WinRmCustomization.ps1' -Recurse -Force
     }
+        if (Test-Path -Path 'C:\Scripts'){
+        Remove-Item 'C:\Scripts' -Recurse -Force
+    }
         if (Test-Path -Path (Join-Path -Path 'C:' -ChildPath $($env:COMPUTERNAME + '.cer') )){
         Remove-Item (Join-Path -Path 'C:' -ChildPath $($env:COMPUTERNAME + '.cer') ) -Recurse -Force
     }
-    Remove-Item -Path "edgeremoval.ps1"
+    if (Test-Path -Path 'edgeremoval.ps1' ){
+        Remove-Item 'edgeremoval.ps1' -Recurse -Force
+    }
 }
 
 $Session = New-LabPSSession -ComputerName (Get-LabVM)
@@ -117,7 +108,7 @@ Remove-PSSession -Session $Session
 Set-LabVMUacStatus -ComputerName (Get-LabVM) -EnableLUA $true -ConsentPromptBehaviorAdmin 5 -ConsentPromptBehaviorUser 3
 
 # Reboot
-$LabClients = Get-LabVM | where {( $_.OperatingSystem -NotLike "*Windows Server*" -and $_.OperatingSystemType -eq "Windows" )}
+$LabClients = Get-LabVM | Where-Object {( $_.OperatingSystem -NotLike "*Windows Server*" -and $_.OperatingSystemType -eq "Windows" )}
 foreach($LabClient in $LabClients)
 {
     Restart-LabVM -ComputerName $LabClient.Name -NoNewLine -Wait
